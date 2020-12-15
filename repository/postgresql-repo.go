cb70168e764be471ef78cdcb1b6f1bf76c50226b
Greg <kretktz@gmail.com>
2020-12-15 14:40:49 +0900

    Abstracted Psql connection

--- repository/postgresql-repo.go
+++ repository/postgresql-repo.go
@@ -21,7 +21,7 @@ const (
 	dbname = "calhounio_demo"
 )
 
-func PostgreConnect() {
+func PsqlConnect() *sql.DB {
 
 	psqlInfo := fmt.Sprintf(
 		"host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
@@ -36,27 +36,19 @@ func PostgreConnect() {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+
+	return db
 }
 
 func (*repo) ListSpacesPsql() ([]entity.ISpace, error){
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db := PsqlConnect()
 
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
 
 	var spaces []entity.ISpace
 
@@ -98,25 +90,16 @@ func (*repo) ListSpacesPsql() ([]entity.ISpace, error){
 }
 
 func (*repo) CreateNewSpacePsql(space *entity.ISpace) (*entity.ISpace, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
+	db := PsqlConnect()
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
 
 	sqlStatement := `
 		INSERT INTO public.ispace (address, availability, coordinates, description, image_urls, location, name, number_of_visitors, telephone_number, top_image_url, uid, visitor_greeting, visitor_slack_message, visitor_slack_webhook_url, website)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
 		RETURNING uid`
 	id := 0
-	err = db.QueryRow(sqlStatement, space.Name, space.Availability, space.Coordinates, space.Description, space.ImageURLS, space.Location, space.Name, space.NumberOfVisitors, space.TelephoneNumber, space.TopImageURL, space.UID, space.VisitorGreeting, space.VisitorSlackMessage, space.VisitorSlackWebhookURL, space.Website).Scan(&id)
+	err := db.QueryRow(sqlStatement, space.Address, space.Availability, space.Coordinates, space.Description, space.ImageURLS, space.Location, space.Name, space.NumberOfVisitors, space.TelephoneNumber, space.TopImageURL, space.UID, space.VisitorGreeting, space.VisitorSlackMessage, space.VisitorSlackWebhookURL, space.Website).Scan(&id)
 	if err != nil {
 		log.Fatalf("Failed to add a new space: %v", err)
 		return nil, err
